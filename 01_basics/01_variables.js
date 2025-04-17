bb404c5e45f9aa5dc129b2509a4efae5c378a13b
chak-de-code <hrreddu8@gmail.com>
2025-04-17 06:44:30 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 147852
+let accountEmail = "nobody@gmail.com"
+var accountPassword = "85769"
+accountCity = "Banglore"
+let accountState;
+
+// accountId = 589647               
+accountEmail = "great@gmail.com"
+accountPassword = "41036897"
+accountCity = "Panipat"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
