fee2171b80e443c28c5837ba836024b8e8eed295
chak-de-code <hrreddu8@gmail.com>
2025-04-17 07:26:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";           // treat all JS code as newer version
+
+// alert(3+3);          // we are using nodejs not browser
+
+// prior readability
+console.log(3
+    +3); console.log("Hello!")
+
+// data types
+/*
+number (range => 2 to power 53)
+bigint
+string =>""
+bool = true / false
+null => standalone value
+undefined => value not assigned
+symbol => unique
+object
+ */
+
+let name = "John"
+let age = 26
+let isloggedIn = false
+
+console.log(typeof age);        // number
+console.log(typeof "hello")     // string
+console.log(typeof undefined)           // undefined
+console.log(typeof null)            // object
